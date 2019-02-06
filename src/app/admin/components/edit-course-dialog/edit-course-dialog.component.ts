3d34e2a72bcb6d1077a9968edb907480a53522b0
Masaki Ihara <m.ihara@lexit.com>
2019-02-06 17:17:46 +0200

    lecture delete

--- src/app/admin/components/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/admin/components/edit-course-dialog/edit-course-dialog.component.ts
@@ -36,14 +36,20 @@ export class EditCourseDialogComponent {
         category: [null, Validators.required],
         imageUrl: [null, Validators.required],
       }),
-      lectures: this.formBuilder.array([])
+      lectures: this.formBuilder.array([]),
+      newLecture: this.formBuilder.group({
+        name: [null, Validators.required],
+        description: [null, Validators.required],
+        videoUrl: [null, Validators.required],
+      }),
     });
     this.course$ = this.courseService.getObservableById(data);
     this.course$.subscribe(course => this.onInit(course));
   }
 
   onInit(course: Course) {
-    this.formGroup.patchValue({ course: course, lectures: [] });
+    this.formGroup.patchValue({ course: course });
+    this.formGroup.setControl('lectures', new FormArray([]));
     const lectures = course.lectures.map(el => this.createLectureGroup(el));
     lectures.forEach(el => (<FormArray>this.formGroup.controls['lectures']).push(el));
 
@@ -57,6 +63,8 @@ export class EditCourseDialogComponent {
   }
 
   onUpdate(_update) {
+    console.log(_update);
+
     const course: Course = {
       id: this.course.id,
       imageUrl: this.course.imageUrl,
@@ -67,31 +75,34 @@ export class EditCourseDialogComponent {
   }
 
   createLectureGroup(data: Lecture): FormGroup {
-    const cleanData = _.omit(data, ['id', 'videoUrl']);
     return this.formBuilder.group({
-      ...cleanData
+      ...data
     });
   }
 
   async onFileSet(file: File) {
     this.downloadURL = await this.storageService.uploadVideo(file);
+    const videoUrl = await this.downloadURL.toPromise();
+    this.formGroup.get('newLecture').patchValue({ videoUrl: videoUrl });
   }
 
-  async addLecture(name = '', description = '') {
-    if (!this.downloadURL) { return; }
-    const videoUrl = await this.downloadURL.toPromise();
+  async addLecture() {
     const id = uuid();
-
-    const lecture: Lecture = { id, name, description, videoUrl };
-
+    const control = this.formGroup.get('newLecture');
+    const lecture: Lecture = { id, ...control.value };
     const currentLectures = _.clone(this.course.lectures);
     const updatedLectures = _.concat(currentLectures, lecture);
 
     const update = _.clone(this.course);
     update.lectures = updatedLectures;
     this.courseService.update(update);
+    control.reset();
   }
 
+  removeLecture(lecture) {
+    _.remove(this.course.lectures, o => o.id === lecture.id);
+    this.courseService.update(this.course);
+  }
   getFormControl(target: string): AbstractControl {
     return this.formGroup.get(target);
   }
