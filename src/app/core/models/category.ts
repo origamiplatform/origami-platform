9fe0e05a7fa1faca9da7f9e45f03e6b03c6ae62a
Masaki Ihara <m.ihara@lexit.co>
2019-02-19 15:13:00 +0200

    wip

--- src/app/core/models/category.ts
+++ src/app/core/models/category.ts
@@ -1,19 +1,22 @@
 export class CategoryNode {
-    id?: string;
-    name: string;
-    level: number;
-    children: CategoryNode[];
+  id?: string;
+  name: string;
+  level: number;
+  children: CategoryNode[];
+  createdBy: string;
 
-    constructor(id?, name?, level?) {
-        id && (this.id = id);
-        name && (this.name = name);
-        level && (this.level = level);
-        this.children = [];
-    }
+  constructor(id?, name?, level?) {
+    id && (this.id = id);
+    name && (this.name = name);
+    level && (this.level = level);
+    this.children = [];
+  }
 }
 
 export class CategoryFlatNode {
-    constructor(
-        public expandable: boolean, public name: string, public level: number) { }
+  constructor(
+    public expandable: boolean,
+    public name: string,
+    public level: number
+  ) {}
 }
-
--- src/app/core/models/course.ts
+++ src/app/core/models/course.ts
@@ -1,15 +1,17 @@
 export interface Course {
-    id?: string;
-    name: string;
-    description: string;
-    lectures: Lecture[];
-    category: string;
-    imageUrl?: string;
+  id?: string;
+  name: string;
+  description: string;
+  lectures: Lecture[];
+  category: string;
+  imageUrl?: string;
+  createdBy: string;
 }
 
 export interface Lecture {
-    id?: string;
-    name: string;
-    description: string;
-    videoUrl: string;
+  id?: string;
+  name: string;
+  description: string;
+  videoUrl: string;
+  createdBy: string;
 }
